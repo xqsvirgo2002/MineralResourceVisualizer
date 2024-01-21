9d47661e9c2e0dafeb0bc81634ccd1f3542b18c8
xqsvirgo2002 <xqsvirgo2002@gmail.com>
2024-01-21 09:18:43 +0800

    Revert "数据可视化大作业"
    This reverts commit 857305a53fca021fc19b63f594f15f2a9dad7bc5.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,10 @@
-// src/main.js
-import Vue from 'vue';
-import App from './App.vue';
-import store from './store';
-import router from './router';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import * as ECharts from 'echarts';
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
 
-Vue.use(ElementUI);
-Vue.prototype.$echarts = ECharts; // 将 ECharts 添加到 Vue 原型链上
-
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   store,
-  router,
   render: h => h(App)
-}).$mount('#app');
+}).$mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -13,6 +13,5 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-  },
-  mode:"history"
+  }
 })
